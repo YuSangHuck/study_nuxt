fdedcae55befc1162a04219966652b2c26b416ec
YuSangHuck <g01099387636@gmail.com>
2019-06-10 12:24:51 +0900

    Add vee-validate config.

--- plugins/vee-validate.js
+++ plugins/vee-validate.js
@@ -1,60 +1,28 @@
-// import Vue from 'vue';
-// // import VeeValidate from 'vee-validate';
-// import VeeValidate, { Validator } from 'vee-validate'
-// // import ko from 'vee-validate/dist/locale/ko.js'
-
-// // // 1. custom rule
-// // const truthyValidator = {
-// //   getMessage(field, args) {
-// //     return 'The ' + field + ' value is not truthy.'
-// //   },
-// //   validate(value, args) {
-// //     return !! value
-// //   }
-// // }
-// // Validator.extend('truthy', truthyValidator)
-// // let instance = new Validator({trueField: 'truthy'})
-
-// const config = {
-//   // locale: ko,
-//   // dictionary: {
-//   //   ko: {
-//   //     // overwriting msg
-//   //     // messages: {
-//   //     //   email: `field: , Some English Msg`
-//   //     // },
-//   //     // attributes === fieldName === inputName
-//   //     attributes: {
-//   //       email: 'E-mail',
-//   //     },
-//   //   }
-//   // },
-//   classes: true,
-//   classNames: {
-//     valid: 'is-valid',
-//     invalid: 'is-invalid'
-//   },
-// }
-// // Vue.use(VeeValidate, config);
-// Vue.use(VeeValidate, config)
-// Validator.localize('ko');
-
 import Vue from 'vue';
-import ar from 'vee-validate/dist/locale/ar';
-// import ko from 'vee-validate/dist/locale/ko';
 import VeeValidate from 'vee-validate';
+// import VeeValidate, { Validator } from 'vee-validate';
+import ko from 'vee-validate/dist/locale/ko';
 
-// // Install the Plugin.
-// Vue.use(VeeValidate);
+const config = {
+  locale: ko.name,
+  dictionary: {
+    // reference: [vee-validate/dist/locale/ko](https://github.com/baianat/vee-validate/blob/master/locale/ko.js)
+    ko: {
+      attributes: {
+        ...ko.attributes,
+        email: '이메일!'
+      },
+      messages: {
+        ...ko.messages,
+        email: (fieldName) => `${fieldName} Some English Message`,
+      }
+    }
+  },
+  classes: true,
+  classNames: {
+    valid: 'is-valid',
+    invalid: 'is-invalid'
+  },
+};
 
-// // Localize takes the locale object as the second argument (optional) and merges it.
-// // Validator.localize('ar', ar);
-// Validator.localize('ko', ko);
-
-Vue.use(VeeValidate, {
-  locale: ar,
-  // dictionary: {
-  //   en: { attributes: attributesEn },
-  //   ar: { messages: messagesAr, attributes: attributesAr }
-  // }
-});
+Vue.use(VeeValidate, config);
