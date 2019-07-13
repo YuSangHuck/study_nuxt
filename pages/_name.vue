dd013159e9a5e1a2543597099c8f7d52e9598fb8
YuSangHuck <g01099387636@gmail.com>
2019-07-14 00:25:35 +0900

    Add dynamic routing,ogtags

---
+++ pages/_name.vue
@@ -0,0 +1,83 @@
+<template>
+  <section class="container">
+    {{info}}
+  </section>
+</template>
+
+<script>
+const infos = {
+  turtlog: {
+    title: '난 turtlog의 title',
+    description: '난 turtlog의 description',
+    image_type: 'image/jpg',
+    image: '/turtlog.jpg',
+    url: 'http://turtlog.ml/turtlog'
+  },
+  whale_shark: {
+    title: '난 whale_shark의 title',
+    description: '난 whale_shark의 description',
+    image_type: 'image/jpg',
+    image: '/whale_shark.jpg',
+    url: 'http://turtlog.ml/whale_shark'
+  },
+}
+export default {
+  data() {
+    return {
+      info: infos[this.$route.params.name]
+    }
+  },
+  // lifecycle hook define
+  validate({params, redirect, app}) {
+    // console.log(infos[params.name])
+    if (infos[params.name] === undefined) redirect('/')
+    else return true
+  },
+  head() {
+    const name = this.$route.params.name
+    return {
+      title: name,
+      meta: [
+        { property: 'og:title', content: infos[name].title, vmid: 'og:title' },
+        { property: 'og:description', content: infos[name].description, vmid: 'og:description' },
+        { property: 'og:image:type', content: infos[name].image_type, vmid: 'og:image:type' },
+        { property: 'og:image', content: infos[name].image, vmid: 'og:image' },
+        { property: 'og:url', content: infos[name].url, vmid: 'og:url' },
+        { property: 'og:type', content: 'website', vmid: 'og:type' },
+      ],
+    };
+  },
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
--- store/index.js
+++ store/index.js
@@ -3,7 +3,7 @@ import getters from './getters'
 
 // https://webpack.js.org/guides/dependency-management/#requirecontext
 const modulesFiles = require.context('./modules', true, /\.js$/)
-console.log(modulesFiles);
+// console.log(modulesFiles);
 // you do not need `import app from './modules/app'`
 // it will auto require all vuex module from modules file
 const modules = modulesFiles.keys().reduce((modules, modulePath) => {
@@ -13,7 +13,7 @@ const modules = modulesFiles.keys().reduce((modules, modulePath) => {
   modules[moduleName] = value.default
   return modules
 }, {})
-console.log(modules);
+// console.log(modules);
 
 const store = () => new Vuex.Store({
   modules,
