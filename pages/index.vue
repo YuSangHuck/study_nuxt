a0acb7ff3df884c2537b4cbb18af35eb867db972
YuSangHuck <g01099387636@gmail.com>
2019-07-13 19:31:04 +0900

    Add test og-tags

--- pages/index.vue
+++ pages/index.vue
@@ -62,6 +62,20 @@ export default {
       getUserID: 'getUserID',
     })
   },
+  head() {
+    // console.log('head called in default');
+    return {
+      title: 'home',
+      meta: [
+        { property: 'og:title', content: 'home title', vmid: 'og:title' },
+        { property: 'og:description', content: 'home description', vmid: 'og:description' },
+        { property: 'og:image:type', content: 'image/jpg', vmid: 'og:image:type' },
+        { property: 'og:image', content: '/og_home.jpg', vmid: 'og:image' },
+        { property: 'og:url', content: 'http://turtlog.ml', vmid: 'og:url' },
+        { property: 'og:type', content: 'website', vmid: 'og:type' },
+      ],
+    };
+  },
   created() {
     // this.$cookiz.set('testCookie', 'i am cookie1!', {
     //   path: '/',
@@ -83,11 +97,11 @@ export default {
     // alert(`process.env: ${JSON.stringify(process.env)}`)
   },
   destroyed() {
-    alert('나 이제 죽는당!')
-    alert(window.localStorage.getItem('vuex'))
+    // alert('나 이제 죽는당!')
+    // alert(window.localStorage.getItem('vuex'))
     window.localStorage.removeItem('vuex')
-    alert('죽었당!')
-    alert(window.localStorage.getItem('vuex'))
+    // alert('죽었당!')
+    // alert(window.localStorage.getItem('vuex'))
   },
   methods: {
     // handleLocalStorage(method) {
